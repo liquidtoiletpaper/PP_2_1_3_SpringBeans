0326ac8adf477a60ce397097fbad005bd0210ff2
tautaste <doritos1488@gmail.com>
2024-02-05 17:26:25 +0900

    reshenie

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,28 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "dog")
+    public Animal getDog() {
+        return new Dog();
+    }
+
+    @Bean(name = "cat")
+    public Animal getCat() {
+        return new Cat();
+    }
+
+    @Bean(name = "time")
+    public Timer getTimer(){
+        return new Timer();
+    }
 }
