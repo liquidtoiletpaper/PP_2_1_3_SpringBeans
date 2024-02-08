a33f8e75b8cab753012b77127e0265f032a9f3dd
tautaste <doritos1488@gmail.com>
2024-02-08 13:01:07 +0900

    Update AnimalsCage.java

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -12,13 +12,14 @@ public class AnimalsCage {
     private Animal animal;
 
     @Autowired
+    @Qualifier("time")
     private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
